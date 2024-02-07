919c29ef609e11a1190217a0b6f06e0f59d1103f
k1ng606 <allenhartley@proton.me>
2024-02-07 19:11:07 +0200

    refactor

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("input_event", input_event_handler)
 	
@@ -9,8 +7,3 @@ func input_event_handler(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		self.queue_free()
 		
-		#print(event)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
